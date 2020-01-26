33feeb9fe85bdde4bdde35ae59ee561d620ded7f
Ramon Carrasco <ramon@ramoncarrasco.es>
2020-01-26 19:16:56 +0100

    ADDED descrption finder

--- src/main/java/es/ramoncarrasco/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/es/ramoncarrasco/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,10 +3,15 @@ package es.ramoncarrasco.spring5recipeapp.repositories;
 import es.ramoncarrasco.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * UnitOfMeasure repository
  * @author Ramón Carrasco
  * Based on Spring Guru RecipeApp https://github.com/springframeworkguru/spring5-recipe-app
  */
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
