b88990329f3e7e9e06c592b2ef013ac157140b92
Ramon Carrasco <ramon@ramoncarrasco.es>
2020-02-02 18:36:24 +0100

    ADDED IndexController unit test

---
+++ src/test/java/es/ramoncarrasco/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,78 @@
+package es.ramoncarrasco.spring5recipeapp.controllers;
+
+import es.ramoncarrasco.spring5recipeapp.domain.Recipe;
+import es.ramoncarrasco.spring5recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexPage() {
+
+        // Given
+        Set<Recipe> recipesData = new HashSet<>();
+
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        recipesData.add(recipe1);
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipesData.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // When
+        String index = indexController.getIndexPage(model);
+
+        // Then
+        assertEquals(index, "index");
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+
+    }
+}
